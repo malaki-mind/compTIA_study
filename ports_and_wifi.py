ab9bd95fc76cba1c099e7af06a490756fcb819cf
Max McMahon <malaki.mind@gmail.com>
2026-09-09 00:55:11 -1000

    completed drills/quizzes for protocols/ports

--- ports_and_wifi.py
+++ ports_and_wifi.py
@@ -37,7 +37,7 @@ ports = { "FTP": {"name" : "File Transfer Protocol (FTP)",
                     "number" : "443"},
          "SMB/CIFS" : {"name" : "Server Message Block (SMB)/Common Internet File System (CIFS)",
                        "number" : "445"},
-         "RDP" : {"name" : "Remote Desktop Protocol",
+         "RDP" : {"name" : "Remote Desktop Protocol (RDP)",
                   "number" : "3389"}
          
          }
@@ -67,19 +67,45 @@ def print_port(port_dict, protocol):
 ## create practice drill functions, IE loop til correct, no scores taken
 ## one for guessing port numbers
 
-port_list = dict_to_list(ports)
-
-def drill(dictionary, question, key_1, key_2):
+def drill(dictionary, question, q, a):
     new_list = dict_to_list(dictionary)
     random.shuffle(new_list)
     user_input = ""
     for item in new_list:
-        user_input = str(input(f"{item[key_1]} {question}\n"))
-        while(user_input != item[key_2]):
+        user_input = str(input(f"{item[q]} {question}\n"))
+        while(user_input != item[a]):
             user_input = str(input(f"wrong\n"))
         print("correct!")
-        
 
+def quiz(dictionary, question, q, a):    
+    new_list = dict_to_list(dictionary)
+    score = 0
+    ceiling = len(new_list)
+    random.shuffle(new_list)
+    user_input = ""
+    for item in new_list:
+        user_input = str(input(f"{item[q]} {question}\n"))
+        if (user_input == item[a]):
+            score += 1
+        else:
+            print("wrong")
+    return score/ceiling * 100
+
+def menu():
+    user_input = 100
+    upper_limit = 6
+    lower_limit = 1
+    print("CompTIA Ports, Protocols, and Wifi")
+    print(f"1. Find Port by Protocol\n2. Port Number Drill")
+    print(f"3. Protocol Drill\n4. Port Number Quiz\n5. Protocol Quiz")
+    print("6. Quit")
+   
+    while ((user_input > upper_limit) or (user_input < lower_limit)):
+        user_input = int(input("Choose an option: "))
+        if ((user_input > upper_limit) or (user_input < lower_limit)):
+            print("invalid response")
+    return user_input
+     
 ## one for guessing port names
 ## one for guessing Wifi frequency
 ## one for guessing Wifi transfer data rate
@@ -87,4 +113,35 @@ def drill(dictionary, question, key_1, key_2):
         
 ## create quiz with scores given, only one shot to answer each question
 ## create menu to choose btwn these options
-drill(ports, "is in which port?", name, number)
+
+class Menu_Options:
+    PORT_FIND = 1
+    PORT_NUM_DRILL = 2
+    PORT_PRO_DRILL = 3
+    PORT_NUM_QUIZ = 4
+    PORT_PRO_QUIZ = 5
+    QUIT = 6
+
+def main():
+
+    menu_choice = -1
+    while(menu_choice != Menu_Options.QUIT):
+        menu_choice = menu()
+        match menu_choice:
+            case Menu_Options.PORT_FIND:
+                user_input = str(input("Enter a Protocol Abbreviation: "))
+                print_port(ports, user_input)
+            case Menu_Options.PORT_NUM_DRILL:
+                drill(ports, "is in which port?", name, number)
+            case Menu_Options.PORT_PRO_DRILL:
+                drill(ports, "has which protocol?", number, name)
+            case Menu_Options.PORT_NUM_QUIZ:
+                print(quiz(ports, "is in which port?", name, number))
+            case Menu_Options.PORT_PRO_QUIZ:
+                print(quiz(ports, "has which protocol?", number, name))
+            case _:
+               print("how?")
+
+#total_score = quiz(ports, "is in which port?", name, number)
+#print(total_score)
+main()
